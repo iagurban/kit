5d8f57ba17ed8d4ddf77ff855f80feee40898da1
Igor Gurban <iagurban@gmail.com>
2025-12-10 15:01:22 +0100

    esm support added v0.0.15

---
+++ .swcrc.cjs
@@ -0,0 +1,8 @@
+{
+  "module": { "type": "commonjs" },
+  "jsc": {
+    "target": "es2019",
+    "parser": { "syntax": "typescript", "tsx": true, "decorators": true },
+    "transform": { "legacyDecorator": true, "decoratorMetadata": true }
+  }
+}
